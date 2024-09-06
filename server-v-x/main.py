c297b98f453c7aee98f617f46f36156d867b2327
Will <williamvdm@live.nl>
2024-09-06 21:10:38 +0200

    removed server from repo

--- server-v-x/main.py
+++ server-v-x/main.py
@@ -20,7 +20,7 @@ print(model.config)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
